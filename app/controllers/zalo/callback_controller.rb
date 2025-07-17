c19cc05f238a81bb10148fa7a50c85be0cda70b8
tralv.1612 <tralv.1612@gmail.com>
2025-07-17 14:22:19 +0700

    update zalo

--- app/controllers/zalo/callback_controller.rb
+++ app/controllers/zalo/callback_controller.rb
@@ -4,9 +4,6 @@ class Zalo::CallbackController < ApplicationController
   def create
     code = params[:code]
     account_id = params[:state]
-    puts "===> code: #{code}"
-    puts "===> account_id: #{account_id}"
-    puts "===> params: #{params.inspect}"
 
     frontend_url = ENV.fetch('FRONTEND_URL', nil)
     redirect_to "#{frontend_url}/app/accounts/#{account_id}/settings/inboxes/new/zalo?code=#{code}"
